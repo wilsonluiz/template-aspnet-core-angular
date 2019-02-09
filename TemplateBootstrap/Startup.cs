4798b3ad1025921b194954af94bc05b5a1cd27cb
Wilson I Luiz <wilson@t2s.com.br>
2019-02-09 10:50:18 -0200

    Habilitado MVC

--- TemplateBootstrap/Startup.cs
+++ TemplateBootstrap/Startup.cs
@@ -15,6 +15,7 @@ namespace TemplateBootstrap
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,6 +27,13 @@ namespace TemplateBootstrap
             }
 
             app.UseStaticFiles();
+
+            app.UseMvc(config =>
+            {
+                config.MapRoute("Default",
+                    "{controller}/{action}/{id?}", 
+                    new { controller = "App", action = "Index"});
+            });
         }
     }
 }
